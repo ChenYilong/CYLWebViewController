fe36c532366409280162120c739f92d99126d3cc
pifupro_iOS_admin <chenyl@pifupro.com>
2015-04-04 16:21:11 +0800

    Initial Commit

---
+++ CYLWebViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CYLWebViewController
+//
+//  Created by chenyilong on 15/4/4.
+//  Copyright (c) 2015年 chenyilong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CYLWebViewController/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CYLWebViewController
+//
+//  Created by chenyilong on 15/4/4.
+//  Copyright (c) 2015年 chenyilong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
